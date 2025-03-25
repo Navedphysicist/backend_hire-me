ef5fd2a2aeeabbe7af52ef657915cb84b15758d3
Naved Khan <navedphsysicist@gmail.com>
2025-03-25 15:58:17 +0530

    Added the seding file for the companies and jobs here

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from db.database import Base, engine
 from routers import auth, company, job, job_application, saved_job, contact
+from seed.seed_jobs import seed_companies_and_jobs
+from seed.seed_companies import seed_companies
 
 # Create database tables
 Base.metadata.create_all(bind=engine)
@@ -20,6 +22,9 @@ app.include_router(job_application.router, tags=["Job Applications"])
 app.include_router(saved_job.router, prefix="/saved-jobs", tags=["Saved Jobs"])
 app.include_router(contact.router, tags=["Contact"])
 
+
+seed_companies_and_jobs()
+seed_companies()
 
 @app.get("/")
 def root():
